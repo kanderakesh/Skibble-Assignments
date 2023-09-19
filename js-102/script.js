af8c0e6749489d7792281f30410196bcd7fba633
kanderakesh <rakeshkande5@gmail.com>
2023-09-19 13:52:05 +0530

    init

---
+++ js-102/script.js
@@ -0,0 +1,41 @@
+let credit = 0;
+let debit = 0;
+let total = 0;
+
+function creditinfo() {
+  let table = document.getElementById("table");
+  let input = document.getElementById("credit-data");
+
+  let rowdata = document.createElement("tr");
+  if (input.value == "" || input.value == undefined) {
+    alert("Please enter the credit value");
+    return;
+  }
+  rowdata.innerHTML = "<b>credited amount:<b>" + input.value;
+  credit = parseFloat(input.value);
+  total = total + credit;
+  console.log(total);
+  table.appendChild(rowdata);
+}
+function debitinfo() {
+  let table1 = document.getElementById("table");
+  let input1 = document.getElementById("debit-data");
+  if (input1.value == "" || input1.value == undefined) {
+    alert("please enter the debit value");
+    return;
+  }
+
+  let rowdata = document.createElement("tr");
+  rowdata.innerHTML = "<b>debited amount:<b>" + input1.value;
+  debit = parseFloat(input1.value);
+  total = total - debit;
+  console.log(total);
+  table1.appendChild(rowdata);
+}
+
+function output() {
+  let rowdata = document.createElement("tr");
+  rowdata.innerHTML = `<h3><b>The Available balance is ${total}</b></h3>`;
+  let table1 = document.getElementById("table");
+  table1.appendChild(rowdata);
+}
